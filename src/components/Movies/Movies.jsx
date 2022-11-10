4ba10f551d1a4a031a87e97a45a1da4a63c6977c
vitaliksmile <vitaliksmile777@gmail.com>
2022-11-10 21:12:00 +0200

    update

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { GetSearchMovies } from '../MoviedApi';
 import { useLocation, useSearchParams } from 'react-router-dom';
-import { Button, Form, Input, Link, ListItem } from './Movies.style';
+import { Button, Form, Input, Link, ListItem } from './Movies.styled';
 const Movies = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [search, setSearch] = useState([]);
